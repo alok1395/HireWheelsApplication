35a7d1b35682c47b7dd4df324c531544e236a362
Alok Yadav <alokyadav95@gmail.com>
2021-02-02 23:27:44 +0530

    Checkpoint 5 Done

---
+++ src/test/java/com/upgrad/hirewheels/services/VehicleServiceTest.java
@@ -0,0 +1,23 @@
+package com.upgrad.hirewheels.services;
+
+import com.upgrad.hirewheels.exceptions.UserNotFoundException;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class VehicleServiceTest {
+
+    @Autowired
+    VehicleService vehicleService;
+
+    @Test
+    public void getAllVehicles() {
+
+    }
+
+    @Test
+    public void getAvailableVehicles() {
+
+    }
+}
